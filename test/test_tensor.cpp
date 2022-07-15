95ca39a66f3d93154c692f03a9451017ab201633
Ivan Malezhyk <ivanmzk@gmail.com>
2022-07-15 22:53:57 +0300

    test tensor spec types as operands

--- test/test_tensor.cpp
+++ test/test_tensor.cpp
@@ -77,6 +77,32 @@ TEST_CASE("test_tensor_construct_using_operator","[test_tensor]"){
     REQUIRE(expr.dim() == expected_dim);
 }
 
+TEST_CASE("test_tensor_construct_using_derived_operands","[test_tensor]"){
+    using value_type = float;
+    using gtensor::tensor;
+    using gtensor::config::default_config;
+    using config_type = default_config<value_type>;
+    using tensor_type = tensor<value_type, default_config>;
+    using shape_type = typename config_type::shape_type;
+    using index_type = typename config_type::index_type;    
+    using test_type = std::tuple<tensor_type, shape_type, index_type, index_type>;
+    //tensor,2expected_shape,3expected size,4expected dim
+    auto test_data = GENERATE(                                
+                                test_type(tensor_type{{1,2,3}}.as_storage_tensor()+tensor_type{{1},{2},{3}}.as_storage_tensor(), shape_type{3,3}, 9 , 2),
+                                test_type((tensor_type{1}+tensor_type{1}).as_expression()+tensor_type{1}.as_storage_tensor() ,shape_type{1}, 1 , 1),
+                                test_type((tensor_type{1,2,3}+tensor_type{{1},{2},{3}}).as_expression()+(tensor_type{1,2,3}+tensor_type{1}).as_expression() ,shape_type{3,3}, 9 , 2)
+                            );
+    
+    auto t = std::get<0>(test_data);        
+    auto expected_shape = std::get<1>(test_data);
+    auto expected_size = std::get<2>(test_data);
+    auto expected_dim = std::get<3>(test_data);        
+    REQUIRE(t.shape() == expected_shape);
+    REQUIRE(t.size() == expected_size);
+    REQUIRE(t.dim() == expected_dim);
+    
+}
+
 TEST_CASE("test_expression_is_trivial","[test_tensor]"){
     using value_type = float;
     using gtensor::tensor;
