22f3237563bda1926257e4c79ca0b65d4ba33228
Ivan Malezhyk <ivanmzk@gmail.com>
2022-07-24 20:55:16 +0300

    refactor view_impl add ptr to view parent remove elements

--- src/impl_view.hpp
+++ src/impl_view.hpp
@@ -7,42 +7,38 @@
 
 namespace gtensor{
 
-template<typename ValT, template<typename> typename Cfg, typename DescT, typename StorT>
+/*
+* ParentT is tensor_impl_base or derived
+*/
+template<typename ValT, template<typename> typename Cfg, typename DescT, typename ParentT>
 class view_impl : public tensor_impl_base<ValT, Cfg> {
     using impl_base_type = tensor_impl_base<ValT,Cfg>;
     using config_type = Cfg<ValT>;        
     using value_type = ValT;
-    using view_storage_type = StorT;
-    using descriptor_type = DescT;
     using index_type = typename config_type::index_type;
     using shape_type = typename config_type::shape_type;
     using storage_type = typename config_type::storage_type;
     using slices_collection_type = typename config_type::slices_collection_type;
-    using walker_factory_type = walker_factory<ValT,Cfg>;
 
-    descriptor_type descriptor;
-    view_storage_type elements;
+    DescT descriptor;
+    std::shared_ptr<ParentT> parent;
     storage_type cache{};
-    walker_factory_type walker_maker;
 
 public:
-    template<typename DtT, typename StT>
-    view_impl(DtT&& descriptor_, StT&& elements_):
+    template<typename DtT>
+    view_impl(DtT&& descriptor_, const std::shared_ptr<ParentT>& parent_):
         descriptor{std::forward<DtT>(descriptor_)},
-        elements{std::forward<StT>(elements_)},
-        walker_maker{*this, descriptor, elements, cache}
+        parent{parent_}
     {}
     view_impl(const view_impl& other):
         descriptor{other.descriptor},
-        elements{other.elements},
-        cache{other.cache},
-        walker_maker{*this, descriptor, elements, cache}
+        parent{other.parent},
+        cache{other.cache}
     {}
     view_impl(view_impl&& other):
         descriptor{std::move(other.descriptor)},
-        elements{std::move(other.elements)},
+        parent{std::move(other.parent)},
         cache{std::move(other.cache)},
-        walker_maker{*this, descriptor, elements, cache}
     {}
 
     index_type size()const override{return descriptor.size();}
@@ -51,7 +47,7 @@ public:
     const shape_type& strides()const override{return descriptor.strides();}
     bool is_cached()const{return cache.size();}
     value_type trivial_at(const index_type& idx)const override{return value_type(0);}
-    walker<ValT,Cfg> create_walker()const override{return walker_maker.create_walker();}
+    walker<ValT,Cfg> create_walker()const override{return nullptr;}
 
     std::shared_ptr<impl_base_type> create_view_slice(const slices_collection_type&)const override{return nullptr;}
     std::shared_ptr<impl_base_type> create_view_transpose(const shape_type&)const override{return nullptr;}
