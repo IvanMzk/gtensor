cd56115e536b2bd7e85905b30a57faa26a4148ae
Ivan Malezhyk <ivanmzk@gmail.com>
2022-07-28 14:27:52 +0300

    refactor vwalker_impl that it can be used in create walker interface remove dependence on descriptor type and storage type make changes to constructor and memebers

--- src/impl_vwalker.hpp
+++ src/impl_vwalker.hpp
@@ -9,32 +9,29 @@ namespace detail{
 }   //end of namespace detail
 
 
-template<typename ValT, template<typename> typename Cfg, typename DescT ,typename StorT>
+template<typename ValT, template<typename> typename Cfg>
 class vwalker_impl : public walker_impl_base<ValT, Cfg>{
     using config_type = Cfg<ValT>;        
     using value_type = ValT;
     using index_type = typename config_type::index_type;
-    using shape_type = typename config_type::shape_type;
-    using storage_type = StorT;
-    using descriptor_type = DescT;
+    using shape_type = typename config_type::shape_type;    
 
-    const descriptor_type* descriptor; 
-    const storage_type* elements;
-    const shape_type* shape{&descriptor->shape()};
-    const shape_type* strides{&descriptor->cstrides()};
-    index_type dim{descriptor->dim()};
-    index_type offset{descriptor->offset()};
+    const shape_type* shape;
+    const shape_type* strides;
+    index_type offset;
+    index_type dim{static_cast<index_type>(shape->size())};
     index_type cursor{offset};
 
     auto shape_element(const index_type direction)const{return (*shape)[direction];}
     auto strides_element(const index_type direction)const{return (*strides)[direction];}
     bool can_walk(const index_type& direction)const{return direction < dim && shape_element(direction) != index_type(1);}
-    std::unique_ptr<walker_impl_base<ValT,Cfg>> clone()const override{return std::make_unique<vwalker_impl<ValT,Cfg,DescT,StorT>>(*this);}
+    std::unique_ptr<walker_impl_base<ValT,Cfg>> clone()const override{return std::make_unique<vwalker_impl<ValT,Cfg>>(*this);}
 
 public:    
-    vwalker_impl(const descriptor_type& descriptor_,  const storage_type& elements_):
-        descriptor{&descriptor_},
-        elements{&elements_}
+    vwalker_impl(const shape_type& shape_,  const shape_type& strides_, const index_type& offset_):
+        shape{&shape_},
+        strides{&strides_},
+        offset{offset_}
     {}
     
     void walk(const index_type& direction, const index_type& steps) override{
@@ -60,10 +57,9 @@ public:
     void reset() override{cursor = offset;}
     
     value_type operator*() const override{        
-        return elements->operator[](descriptor->convert_by_prev(cursor));
+        return 0;
     }
 };
-
 
 }   //end of namespace gtensor
 
