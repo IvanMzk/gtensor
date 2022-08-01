51eca882e07b26f4ff796e2d7b00d2c36b1dfc93
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-01 21:14:00 +0300

    modify view_impl implement view_expression_walker_impl making interface add view_root_kind method to use in dispatcher

--- src/impl_view.hpp
+++ src/impl_view.hpp
@@ -19,7 +19,8 @@ class view_impl :
     public tensor_impl_base<ValT, Cfg>,
     public view_impl_base<ValT,Cfg>,
     public storage_tensor_impl_base<ValT,Cfg>,
-    public view_index_converter<ValT,Cfg>
+    public view_index_converter<ValT,Cfg>,
+    public view_expression_impl_base<ValT,Cfg>
 {
     using impl_base_type = tensor_impl_base<ValT,Cfg>;
     using config_type = Cfg<ValT>;        
@@ -45,9 +46,14 @@ class view_impl :
             throw view_impl_exception("storage_walker cant be created, view not cached and parent not storage");
         }
     }
-    vwalker_impl<ValT,Cfg> create_view_walker()const override{
-        return vwalker_impl<ValT,Cfg>{shape(),descriptor.cstrides(),descriptor.offset()};
+    view_expression_walker_impl<ValT,Cfg> create_view_expression_walker()const override{
+        return view_expression_walker_impl<ValT,Cfg>{shape(),descriptor.cstrides(),descriptor.offset(), parent_converter, view_root->as_expression()->create_storage()};        
     }
+    
+    const storage_tensor_impl_base<ValT,Cfg>* as_storage_tensor()const override{return static_cast<const storage_tensor_impl_base<ValT,Cfg>*>(this);}
+    const view_impl_base<ValT,Cfg>* as_view()const override{return static_cast<const view_impl_base<ValT,Cfg>*>(this);}
+    const view_index_converter<ValT,Cfg>* as_index_converter()const override{return static_cast<const view_index_converter<ValT,Cfg>*>(this);}
+    const view_expression_impl_base<ValT,Cfg>* as_view_expression()const{return static_cast<const view_expression_impl_base<ValT,Cfg>*>(this);}
     
     bool is_storage_parent()const{
         return 
@@ -55,12 +61,10 @@ class view_impl :
             parent->tensor_kind() == detail::tensor_kinds::expression && parent->is_storage() ||
             parent->tensor_kind() == detail::tensor_kinds::view && parent->as_view()->is_cached();
     }
-    const storage_tensor_impl_base<ValT,Cfg>* as_storage_tensor()const override{return static_cast<const storage_tensor_impl_base<ValT,Cfg>*>(this);}
-    const view_impl_base<ValT,Cfg>* as_view()const override{return static_cast<const view_impl_base<ValT,Cfg>*>(this);}
-    const view_index_converter<ValT,Cfg>* as_index_converter()const override{return static_cast<const view_index_converter<ValT,Cfg>*>(this);}
 
     index_type view_index_convert(const index_type& idx)const override{return parent_converter->convert(descriptor.convert(idx));}
-    bool is_cached()const override{return cache.size();}    
+    bool is_cached()const override{return cache.size();}
+    detail::tensor_kinds view_root_kind()const override{return view_root->tensor_kind();}
     bool is_storage()const override{return is_storage_parent() || is_cached();}
     bool is_trivial()const override{return true;}
     const value_type* storage_data()const override{return cache.data();}
