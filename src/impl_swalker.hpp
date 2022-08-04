38aa9209cf27626f87b6379c4381e689474e6004
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-04 12:59:48 +0300

    refactor storage walker make it inherite from basic_walker

--- src/impl_swalker.hpp
+++ src/impl_swalker.hpp
@@ -10,27 +10,24 @@ namespace detail{
 
 
 template<typename ValT, template<typename> typename Cfg>
-class storage_walker_impl : public walker_impl_base<ValT, Cfg>{
+class storage_walker_impl : 
+    public walker_impl_base<ValT, Cfg>,
+    private basic_walker<ValT,Cfg>
+{   
+    using basic_type = basic_walker<ValT,Cfg>;
     using config_type = Cfg<ValT>;        
     using value_type = ValT;
     using index_type = typename config_type::index_type;
     using shape_type = typename config_type::shape_type;
-
-    const shape_type* shape;
-    const shape_type* strides;
+    
     const value_type* data;
-    index_type dim{static_cast<index_type>(shape->size())};
     const value_type* cursor{data};
 
-    auto shape_element(const index_type direction)const{return (*shape)[direction];}
-    auto strides_element(const index_type direction)const{return (*strides)[direction];}
-    bool can_walk(const index_type& direction)const{return direction < dim && shape_element(direction) != index_type(1);}
     std::unique_ptr<walker_impl_base<ValT,Cfg>> clone()const override{return std::make_unique<storage_walker_impl<ValT,Cfg>>(*this);}
 
 public:    
     storage_walker_impl(const shape_type& shape_, const shape_type& strides_,  const value_type* data_):
-        shape{&shape_},
-        strides{&strides_},        
+        basic_type{static_cast<index_type>(shape_.size()), shape_, strides_},
         data{data_}
     {}
     
