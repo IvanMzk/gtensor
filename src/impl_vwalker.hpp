64655a07c290c221bfa1a59ccaa4fba8ae7399e8
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-04 23:13:08 +0300

    refactor vwalker_impl inherite from basic_walker

--- src/impl_vwalker.hpp
+++ src/impl_vwalker.hpp
@@ -9,67 +9,13 @@ namespace gtensor{
 namespace detail{
 }   //end of namespace detail
 
-/*
-* abstract base class for view of expression walker class, view of view walker class
-* operator* not implemented
-*/
-template<typename ValT, template<typename> typename Cfg>
-class vwalker_impl
-{
-    using config_type = Cfg<ValT>;        
-    using value_type = ValT;
-    using index_type = typename config_type::index_type;
-    using shape_type = typename config_type::shape_type;    
-
-    const shape_type* shape;
-    const shape_type* strides;
-    index_type offset;
-    index_type dim{static_cast<index_type>(shape->size())};
-    index_type cursor{offset};
-
-    auto shape_element(const index_type direction)const{return (*shape)[direction];}
-    auto strides_element(const index_type direction)const{return (*strides)[direction];}
-    bool can_walk(const index_type& direction)const{return direction < dim && shape_element(direction) != index_type(1);}
-
-protected:
-    
-    void walk(const index_type& direction, const index_type& steps){
-        if (can_walk(direction)){
-            cursor+=steps*strides_element(direction);
-        }   
-    }
-    void step(const index_type& direction){
-        if (can_walk(direction)){
-            cursor+=strides_element(direction);
-        }
-    }
-    void step_back(const index_type& direction){        
-        if (can_walk(direction)){
-            cursor-=strides_element(direction);
-        }            
-    }
-    void reset(const index_type& direction){
-        if (can_walk(direction)){
-            cursor-=(shape_element(direction)-1)*strides_element(direction);
-        }
-    }
-    void reset(){cursor = offset;}    
-    index_type get_cursor()const{return cursor;}
-
-public:    
-    vwalker_impl(const shape_type& shape_,  const shape_type& strides_, const index_type& offset_):
-        shape{&shape_},
-        strides{&strides_},
-        offset{offset_}
-    {}
-};
 
 template<typename ValT, template<typename> typename Cfg>
 class view_expression_walker_impl : 
     public walker_impl_base<ValT, Cfg>,
-    private vwalker_impl<ValT, Cfg>
+    private basic_walker<ValT, Cfg, typename Cfg<ValT>::index_type>
 {
-    using base_type = vwalker_impl<ValT, Cfg>;    
+    using base_basic_walker = basic_walker<ValT, Cfg, typename Cfg<ValT>::index_type>;
     using config_type = Cfg<ValT>;        
     using value_type = ValT;
     using index_type = typename config_type::index_type;
@@ -81,18 +27,18 @@ class view_expression_walker_impl :
 
 public:        
     view_expression_walker_impl(const shape_type& shape_,  const shape_type& strides_, const index_type& offset_, const view_index_converter<ValT,Cfg>* converter_, evaluating_storage<ValT,Cfg> estorage_):
-        base_type{shape_,strides_,offset_},
+        base_basic_walker{static_cast<index_type>(shape_.size()), shape_, strides_, offset_},
         converter{converter_},
         estorage{std::move(estorage_)}
     {}    
     
-    void walk(const index_type& direction, const index_type& steps)override{base_type::walk(direction,steps);}
-    void step(const index_type& direction)override{base_type::step(direction);}
-    void step_back(const index_type& direction)override{base_type::step_back(direction);}
-    void reset(const index_type& direction)override{base_type::reset(direction);}
-    void reset()override{base_type::reset();}    
+    void walk(const index_type& direction, const index_type& steps)override{base_basic_walker::walk(direction,steps);}
+    void step(const index_type& direction)override{base_basic_walker::step(direction);}
+    void step_back(const index_type& direction)override{base_basic_walker::step_back(direction);}
+    void reset(const index_type& direction)override{base_basic_walker::reset(direction);}
+    void reset()override{base_basic_walker::reset();}    
     value_type operator*()const override{        
-        return estorage[converter->convert(base_type::get_cursor())];
+        return estorage[converter->convert(base_basic_walker::cursor())];
     }
 };
 
