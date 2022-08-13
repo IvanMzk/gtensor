38a5e12d453371f2dcb03f6887cd845863a7c292
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-13 16:27:27 +0300

    refactor storage_walker_polymorphic make it derived from storage_walker as implementation remove derivation from basic_walker

--- src/storage_walker.hpp
+++ src/storage_walker.hpp
@@ -33,9 +33,9 @@ public:
 template<typename ValT, template<typename> typename Cfg>
 class storage_walker_polymorphic : 
     public walker_base<ValT, Cfg>,
-    private basic_walker<ValT,Cfg, const ValT*>
+    private storage_walker<ValT,Cfg>
 {   
-    using base_basic_walker = basic_walker<ValT, Cfg, const ValT*>;
+    using base_storage_walker = storage_walker<ValT, Cfg>;
     using config_type = Cfg<ValT>;        
     using value_type = ValT;
     using index_type = typename config_type::index_type;
@@ -45,14 +45,14 @@ class storage_walker_polymorphic :
 
 public:    
     storage_walker_polymorphic(const shape_type& shape_, const shape_type& strides_,  const value_type* data_):
-        base_basic_walker{static_cast<index_type>(shape_.size()), shape_, strides_, data_}        
+        base_storage_walker{shape_, strides_, data_}        
     {}
-    void walk(const index_type& direction, const index_type& steps)override{base_basic_walker::walk(direction,steps);}
-    void step(const index_type& direction)override{base_basic_walker::step(direction);}
-    void step_back(const index_type& direction)override{base_basic_walker::step_back(direction);}
-    void reset(const index_type& direction)override{base_basic_walker::reset(direction);}
-    void reset()override{base_basic_walker::reset();}
-    value_type operator*() const override{return *cursor();}
+    void walk(const index_type& direction, const index_type& steps)override{base_storage_walker::walk(direction,steps);}
+    void step(const index_type& direction)override{base_storage_walker::step(direction);}
+    void step_back(const index_type& direction)override{base_storage_walker::step_back(direction);}
+    void reset(const index_type& direction)override{base_storage_walker::reset(direction);}
+    void reset()override{base_storage_walker::reset();}
+    value_type operator*() const override{return base_storage_walker::operator*();}
 };
 
 }   //end of namespace gtensor
