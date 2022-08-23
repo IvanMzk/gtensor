e87e4b0bc804b94d74cf3690d5c26b7f4e21d8a2
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-23 20:38:26 +0300

    make operation macros more readeble

--- src/operations.hpp
+++ src/operations.hpp
@@ -7,7 +7,12 @@ namespace gtensor{
 
 namespace binary_operations{
 #define BINARY_OPERATION(NAME, OP)\
-struct NAME{template <typename T1, typename T2> auto operator()(T1&& arg1, T2&& arg2) const {return (std::forward<T1>(arg1) OP std::forward<T2>(arg2));}};
+struct NAME{\
+    template <typename T1, typename T2>\
+    auto operator()(T1&& arg1, T2&& arg2) const {\
+        return (std::forward<T1>(arg1) OP std::forward<T2>(arg2));\
+    }\
+};
 
 BINARY_OPERATION(add,+);
 BINARY_OPERATION(sub,-);
@@ -22,7 +27,12 @@ BINARY_OPERATION(logic_or,||);
 
 namespace unary_operations{
 #define UNARY_OPERATION(NAME, OP)\
-struct NAME{template <typename T> auto operator()(T&& arg) const {return (OP std::forward<T>(arg));}};
+struct NAME{\
+    template <typename T>\
+    auto operator()(T&& arg) const {\
+        return (OP std::forward<T>(arg));\
+    }\
+};
 
 UNARY_OPERATION(ident,+);
 UNARY_OPERATION(neg,-);
