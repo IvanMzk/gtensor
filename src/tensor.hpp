6f63c38227ac0ae7a1dd4e78e6c169a749cf443b
Ivan Malezhyk <ivanmzk@gmail.com>
2022-08-27 22:21:36 +0300

    refactor tensor add create_view_helper to make tensor with right impl type refactor view making methods to use helper test

--- src/tensor.hpp
+++ src/tensor.hpp
@@ -10,14 +10,16 @@
 
 namespace gtensor{
 
+namespace detail{
+}   //end of namespace detail
+
 /*
 * tensor is abstraction of stensor_impl, expression or view which are implementations
 * in aplication client use tensor abstraction objects with different implementations, can combine it using operators
 * client can evaluate tensor object with expression or view implementation to have tensor with stensor_impl implementation
 */
 template<typename ValT, typename CfgT, typename ImplT = storage_tensor<ValT, CfgT>>
-class tensor{
-    using tensor_type = tensor<ValT,CfgT>;    
+class tensor{    
     using tensor_base_type = tensor_base<ValT, CfgT>;
     using htensor_type = tensor<ValT, CfgT, tensor_base_type>;
     using impl_type = ImplT;
@@ -38,7 +40,9 @@ class tensor{
         impl_{std::make_shared<impl_type>(init_data)}
     {}
 
-    
+    template<typename T>
+    auto create_view_helper(std::shared_ptr<T>&& v)const{return tensor<ValT, CfgT, T>{std::move(v)};}
+
 protected:
     auto impl()const{return impl_;}
 
@@ -76,37 +80,37 @@ public:
     // auto as_expression()const{return expression<ValT,CfgT>{*this};}
     // auto as_storage_tensor()const{return storage_tensor<ValT,CfgT>{*this};}
 
-    tensor_type operator()(slices_init_type subs)const{
+    auto operator()(slices_init_type subs)const{
         detail::check_slices_number(subs);        
         slices_collection_type filled_subs = detail::fill_slices<slice_type>(shape(),subs);
         detail::check_slices(shape(), filled_subs);        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_slice(impl(), filled_subs)};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_slice(impl(), filled_subs));
     }
     template<typename...Subs, std::enable_if_t<std::conjunction_v<std::is_convertible<Subs,slice_type>...>,int> = 0 >
-    tensor_type operator()(const Subs&...subs)const{
+    auto operator()(const Subs&...subs)const{
         detail::check_slices_number(subs...);
         slices_collection_type filled_subs = detail::fill_slices<slice_type>(shape(),subs...);
         detail::check_slices(shape(), filled_subs);        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_slice(impl(), filled_subs)};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_slice(impl(), filled_subs));
     }
     template<typename...Subs, std::enable_if_t<std::conjunction_v<std::is_convertible<Subs,index_type>...>,int> = 0 >
-    tensor_type transpose(const Subs&...subs)const{
+    auto transpose(const Subs&...subs)const{
         detail::check_transpose_subs(dim(),subs...);        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_transpose(impl(), shape_type{subs...})};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_transpose(impl(), shape_type{subs...}));
     }
     template<typename...Subs, std::enable_if_t<std::conjunction_v<std::is_convertible<Subs,index_type>...>,int> = 0 >
-    tensor_type operator()(const Subs&...subs)const{
+    auto operator()(const Subs&...subs)const{
         detail::check_subdim_subs(shape(), subs...);        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_subdim(impl(), shape_type{subs...})};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_subdim(impl(), shape_type{subs...}));
     }            
-    tensor_type operator()()const{
+    auto operator()()const{
         detail::check_subdim_subs(shape());        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_subdim(impl(), shape_type{})};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_subdim(impl(), shape_type{}));
     }        
     template<typename...Subs, std::enable_if_t<std::conjunction_v<std::is_convertible<Subs,index_type>...>,int> = 0 >
-    tensor_type reshape(const Subs&...subs)const{
+    auto reshape(const Subs&...subs)const{
         detail::check_reshape_subs(size(), subs...);        
-        return tensor_type{view_factory<ValT,CfgT>::create_view_reshape(impl(), shape_type{subs...})};
+        return create_view_helper(view_factory<ValT,CfgT>::create_view_reshape(impl(), shape_type{subs...}));
     }
     
 private:
