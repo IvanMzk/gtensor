ccd860dd91076aa5220eba44ee1006e6c1d15cd8
Ivan Malezhyk <ivanmzk@gmail.com>
2022-09-10 22:08:20 +0300

    add engine_host_accessor, storage_engine and evaluating_engine classes

--- src/engine_base.hpp
+++ src/engine_base.hpp
@@ -24,31 +24,11 @@ struct engine_traits<evaluating_tensor<ValT, CfgT, F, Ops...>>{using type = expr
 }   //end of namespace detail
 
 
-// template<typename EngineT, typename ValT, typename CfgT, typename F, typename...Ops>
-// class evaluating_engine_root_accessor
-// {        
-//     using root_type = evaluating_tensor<ValT, CfgT, F, EngineT, Ops...>;    
-//     root_type* root_{nullptr};
-//     friend root_type;
-//     void set_root(tensor_base<ValT,CfgT>* root__){root_ = static_cast<root_type*>(root__);}
-// protected:
-//     auto root()const{return root_;}
-// };
-
-template<template<typename...> typename RootT, typename...Args>
-class engine_root_accessor
-{        
-    using root_type = RootT<Args...>;    
-    root_type* root_{nullptr};
-    friend root_type;
-    void set_root(root_type* root__){root_ = root__;}
-protected:
-    auto root()const{return root_;}    
-    engine_root_accessor() = default;
-    engine_root_accessor(root_type* root__):
-        root_{root__}
-    {}
-};
+/**
+ * 
+ * engines are about meaning not form, as opposed to descriptors that are about form
+ * 
+ * **/
 
 
 template<typename ValT, typename CfgT>
@@ -56,6 +36,61 @@ class expression_template_engine_base{
 public:
     virtual bool is_trivial()const = 0;
 };
+
+template<typename ValT, typename CfgT>
+class engine_host_accessor
+{        
+protected:
+    using host_type = tensor_base<ValT,CfgT>;
+    host_type* host_{nullptr};    
+    engine_host_accessor() = default;
+    engine_host_accessor(host_type* host__):
+        host_{host__}
+    {}
+    void set_host(host_type* host__){host_ = host__;}
+    auto host()const{return host_;}    
+};
+
+
+template<typename ValT, typename CfgT>
+class storage_engine : 
+    protected engine_host_accessor<ValT, CfgT>
+{
+    using engine_host_accessor::host_type;
+    using storage_type = typename CfgT::template storage<ValT>;    
+    storage_type elements_;
+public:    
+    explicit storage_engine(storage_type&& elements):        
+        elements_{std::move(elements)}
+    {}
+    storage_engine(host_type* host, storage_type&& elements):
+        engine_host_accessor{host},
+        elements_{std::move(elements)}
+    {}
+};
+
+template<typename ValT, typename CfgT, typename F, typename...Ops>
+class evaluating_engine : 
+    protected engine_host_accessor<ValT, CfgT>
+{
+    using engine_host_accessor::host_type;
+    std::tuple<std::shared_ptr<Ops>...> operands_;
+public:
+    template<typename...Args, std::enable_if_t<sizeof...(Args)==sizeof...(Ops),int> = 0 >
+    explicit evaluating_engine(Args&&...args):
+        operands_{std::forward<Args>(args)...}
+    {}
+    template<typename...Args>
+    explicit evaluating_engine(host_type* host, Args&&...args):
+        operands_{std::forward<Args>(args)...},
+        engine_host_accessor{host}
+    {}
+};
+
+
+
+
+
 
 }   //end of namespace gtensor
 
