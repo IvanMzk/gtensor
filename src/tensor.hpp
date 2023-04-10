f5ede6add4a3374f9f66bf7a32fcde74ca8d38b9
Ivan Malezhyk <ivanmzk@gmail.com>
2023-04-10 12:00:13 +0300

    refactor tensor make tensor_crtp_base owns descriptor

--- src/tensor.hpp
+++ src/tensor.hpp
@@ -8,18 +8,26 @@
 
 namespace gtensor{
 
-template<typename Impl>
+//base type for all tensor implementations
+//Impl - tensor implementation type
+//Descriptor - descriptor type used by tensor implementation
+//Impl must provide interface to make data accessor:
+//create_indexer() or create_walker() or both, if both is proveded type alias tag Impl::data_accessor_tag is used to select accessor
+template<typename Impl, typename Descriptor>
 class tensor_crtp_base
 {
     using impl_type = Impl;
-    using descriptor_type = typename impl_type::descriptor_type;
     static_assert(std::is_convertible_v<impl_type*,const tensor_crtp_base*>);
 
     const impl_type& impl()const{return static_cast<const impl_type&>(*this);}
     impl_type& impl(){return static_cast<impl_type&>(*this);}
 protected:
-    tensor_crtp_base(){}
+    using descriptor_type = Descriptor;
     ~tensor_crtp_base(){}
+    template<typename...Args>
+    tensor_crtp_base(Args&&...args):
+        descriptor_{std::forward<Args>(args)...}
+    {}
 public:
     using value_type = typename impl_type::value_type;
     using config_type = typename impl_type::config_type;
@@ -36,59 +44,26 @@ public:
     tensor_crtp_base(tensor_crtp_base&&) = delete;
     tensor_crtp_base& operator=(tensor_crtp_base&&) = delete;
 
-    const descriptor_type& descriptor()const{return impl().get_descriptor();}
-    descriptor_type& descriptor(){return impl().get_descriptor();}
-    index_type size()const{return descriptor().size();}
-    dim_type dim()const{return descriptor().dim();}
-    const shape_type& shape()const{return descriptor().shape();}
-    const shape_type& strides()const{return descriptor().strides();}
+    const descriptor_type& descriptor()const{return descriptor_;}
+    descriptor_type& descriptor(){return descriptor_;}
+    index_type size()const{return descriptor_.size();}
+    dim_type dim()const{return descriptor_.dim();}
+    const shape_type& shape()const{return descriptor_.shape();}
+    const shape_type& strides()const{return descriptor_.strides();}
+
+private:
+    descriptor_type descriptor_;
 };
-// template<typename DescT, typename EngineT>
-// class basic_tensor : public tensor_base<typename EngineT::value_type, typename EngineT::config_type>
-// {
-//     using tensor_base_type = tensor_base<typename EngineT::value_type, typename EngineT::config_type>;
-// protected:
-//     using descriptor_type = DescT;
-//     using engine_type = EngineT;
-//     basic_tensor(engine_type&& engine__,descriptor_type&& descriptor__):
-//         engine_{std::move(engine__)},
-//         descriptor_{std::move(descriptor__)}
-//     {}
-// public:
-//     using typename tensor_base_type::value_type;
-//     using typename tensor_base_type::config_type;
-//     using typename tensor_base_type::dim_type;
-//     using typename tensor_base_type::index_type;
-//     using typename tensor_base_type::shape_type;
 
-//     basic_tensor(const basic_tensor&) = delete;
-//     basic_tensor& operator=(const basic_tensor&) = delete;
-//     basic_tensor(basic_tensor&&) = delete;
-//     basic_tensor& operator=(basic_tensor&&) = delete;
-
-//     const descriptor_type& descriptor()const override{return descriptor_;}
-//     descriptor_type& descriptor()override{return descriptor_;}
-//     index_type size()const override{return descriptor_.size();}
-//     dim_type dim()const override{return descriptor_.dim();}
-//     const shape_type& shape()const override{return descriptor_.shape();}
-//     const shape_type& strides()const override{return descriptor_.strides();}
-
-//     const engine_type& engine()const{return engine_;}
-//     engine_type& engine(){return engine_;}
-
-// private:
-//     engine_type engine_;
-//     descriptor_type descriptor_;
-// };
-
+//tensor implementation that owns storage of data elements of type ValT
+//storage type is specialization of CfgT::storage template
 template<typename CfgT, typename ValT>
-class storage_tensor : public tensor_crtp_base<storage_tensor<CfgT,ValT>>
+class storage_tensor : public tensor_crtp_base<storage_tensor<CfgT,ValT>,basic_descriptor<CfgT>>
 {
-    using tensor_crtp_base_type = tensor_crtp_base<storage_tensor<CfgT,ValT>>;
+    using tensor_crtp_base_type = tensor_crtp_base<storage_tensor<CfgT,ValT>,basic_descriptor<CfgT>>;
 public:
     using value_type = ValT;
     using config_type = config::extend_config_t<CfgT, ValT>;
-    using descriptor_type = basic_descriptor<config_type>;
     using storage_type = typename config_type::storage_type
     using dim_type = typename config_type::dim_type;
     using index_type = typename config_type::index_type;
@@ -101,19 +76,19 @@ public:
 
     template<typename Nested>
     storage_tensor(std::initializer_list<Nested> init_data):
-        descriptor_{detail::list_parse<dim_type,shape_type>(init_data)},
+        tensor_crtp_base_type{detail::list_parse<dim_type,shape_type>(init_data)},
         elements_(descriptor_.size())
     {
         detail::fill_from_list(init_data, elements_.begin());
     }
     template<typename ShT>
     storage_tensor(ShT&& shape, const value_type& v):
-        descriptor_{std::forward<ShT>(shape)},
+        tensor_crtp_base_type{std::forward<ShT>(shape)},
         elements_(descriptor_.size(),v)
     {}
     template<typename ShT, typename ItT>
     storage_tensor(ShT&& shape, ItT begin, ItT end):
-        descriptor_{std::forward<ShT>(shape)},
+        tensor_crtp_base_type{std::forward<ShT>(shape)},
         elements_(descriptor_.size())
     {
         index_type n = std::distance(begin,end);
@@ -126,17 +101,16 @@ public:
         }
     }
 
-    const auto& get_descriptor()const{return descriptor_;}
-    auto& get_descriptor(){return descriptor_;}
+
     //inplace
     template<typename ShT>
     void resize(ShT&& shape){
-        descriptor_ = descriptor_type{std::forward<ShT>(shape)};
+        using typename tensor_crtp_base_type::descriptor_type;
+        tensor_crtp_base_type::descriptor() = descriptor_type{std::forward<ShT>(shape)};
         elements_.resize(descriptor_.size());
         elements_.shrink_to_fit();
     }
 private:
-    descriptor_type descriptor_;
     storage_type elements_;
 };
 
