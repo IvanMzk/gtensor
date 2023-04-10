d3b0b8d2a92c175929bd46d0eb369c4de2058bea
Ivan Malezhyk <ivanmzk@gmail.com>
2023-04-10 22:11:28 +0300

    add data_accessor module add basic_indexer

--- src/data_accessor.hpp
+++ src/data_accessor.hpp
@@ -5,58 +5,64 @@
 
 namespace gtensor{
 
-//basic_indexer provides interface for random access of underlaying storage using subscription operator
-//for storage engine it may wrapps its iterator, random access iterator required
-//for viewing engine it is wrapper around its parent's indexer and use descriptor for index mapping
-//subscription operator result can be used for assigning, if underlaying indexer's subscription result allows it
+//basic indexer is data accessor that uses flat index to address data
+//indexers can be chained to make data view
 template<typename...> class basic_indexer;
+//Should be the first indexer in chain
+//Parent is data storage that provide subscript operator
+template<typename Parent>
+class basic_indexer<Parent&>
+{
+    using parent_type = Parent;
+    parent_type* parent_;
+public:
+    template<typename Parent_, std::enable_if_t<std::is_lvalue_reference_v<Parent_> && !std::is_convertible_v<std::decay_t<Parent_>, basic_indexer>,int> =0>
+    explicit basic_indexer(Parent_&& parent__):
+        parent_{&parent__}
+    {}
+    template<typename U>
+    decltype(std::declval<parent_type>()[std::declval<U>()]) operator[](const U& i)const{
+        return (*parent_)[i];
+    }
+};
+//shouldn't be the first indexer in the chain
+//Indexer is type of previous indexer in the chain
 template<typename Indexer>
-class basic_indexer<Indexer>{
+class basic_indexer<Indexer>
+{
+    static_assert(!std::is_reference_v<Indexer>);
     using indexer_type = Indexer;
     indexer_type indexer_;
 public:
-    template<typename Indexer_>
-    basic_indexer(Indexer_&& indexer__):
+    template<typename Indexer_, std::enable_if_t<!std::is_convertible_v<std::decay_t<Indexer_>, basic_indexer>,int> =0>
+    explicit basic_indexer(Indexer_&& indexer__):
         indexer_{std::forward<Indexer_>(indexer__)}
     {}
     template<typename U>
-    decltype(std::declval<indexer_type>()[std::declval<U>()]) operator[](const U& i){
+    decltype(std::declval<indexer_type>()[std::declval<U>()]) operator[](const U& i)const{
         return indexer_[i];
     }
 };
-
-template<typename Indexer>
-class basic_indexer<Indexer&>{
-    using indexer_type = Indexer;
-    indexer_type* indexer_;
-public:
-    basic_indexer(indexer_type& indexer__):
-        indexer_{&indexer__}
-    {}
-    template<typename U>
-    decltype(std::declval<indexer_type>()[std::declval<U>()]) operator[](const U& i){
-        return (*indexer_)[i];
-    }
-};
-
-template<typename Indexer, typename Descriptor>
-class basic_indexer<Indexer, Descriptor> : public basic_indexer<Indexer>
+//map data elements using converter
+//Indexer is type of previous indexer in the chain or data storage
+//Converter is flat index mapper, must provide operator()() that take index as parameter and return mapped index
+template<typename Indexer, typename Converter>
+class basic_indexer<Indexer, Converter> : public basic_indexer<Indexer>
 {
     using basic_indexer_base = basic_indexer<Indexer>;
-    using descriptor_type = Descriptor;
+    using Converter_type = Converter;
 public:
-    using typename basic_indexer_base::indexer_type;
     template<typename Indexer_>
-    basic_indexer(Indexer_&& indexer__, const descriptor_type& converter__):
+    basic_indexer(Indexer_&& indexer__, const Converter_type& converter__):
         basic_indexer_base{std::forward<Indexer_>(indexer__)},
         converter_{&converter__}
     {}
     template<typename U>
-    decltype(std::declval<basic_indexer_base>()[std::declval<descriptor_type>().convert(std::declval<U>())]) operator[](const U& i){
-        return basic_indexer_base::operator[](converter_->convert(i));
+    decltype(std::declval<basic_indexer_base>()[std::declval<Converter_type>().operator()(std::declval<U>())]) operator[](const U& i)const{
+        return basic_indexer_base::operator[](converter_->operator()(i));
     }
 private:
-    const descriptor_type* converter_;
+    const Converter_type* converter_;
 };
 
 
--- test/test_data_accessor.cpp
+++ test/test_data_accessor.cpp
@@ -1,7 +1,92 @@
 #include "catch.hpp"
 #include "descriptor.hpp"
 #include "data_accessor.hpp"
+#include "helpers_for_testing.hpp"
 
-TEST_CASE("test_basic_indexer","test_data_accessor"){
+TEST_CASE("test_basic_indexer_reference_specialization","test_data_accessor"){
+    using gtensor::basic_indexer;
+    using helpers_for_testing::apply_by_element;
 
+    //0parent,1index,2expected
+    auto test_data = std::make_tuple(
+        std::make_tuple(std::vector<int>{1,2,3,0}, 0, 1),
+        std::make_tuple(std::vector<int>{1,2,3,0}, 1, 2),
+        std::make_tuple(std::vector<int>{1,2,3,0}, 2, 3),
+        std::make_tuple(std::vector<int>{1,2,3,0}, 3, 0)
+    );
+    auto test = [](const auto& t, auto& parent){
+        auto index = std::get<1>(t);
+        auto expected = std::get<2>(t);
+        using parent_type = decltype(parent);
+        auto indexer = basic_indexer<parent_type>{parent};
+        auto result = indexer[index];
+        REQUIRE(std::is_same_v<decltype(parent[index]),decltype(indexer[index])>);
+        REQUIRE(result == expected);
+    };
+    SECTION("test_basic_indexer_reference_to_non_const_specialization")
+    {
+        auto test_ = [test](const auto& t){
+            auto parent = std::get<0>(t);
+            using parent_type = decltype(parent);
+            static_assert(!std::is_const_v<parent_type>);
+            test(t,parent);
+        };
+        apply_by_element(test_, test_data);
+    }
+    SECTION("test_basic_indexer_reference_to_const_specialization")
+    {
+        auto test_ = [test](const auto& t){
+            const auto parent = std::get<0>(t);
+            using parent_type = decltype(parent);
+            static_assert(std::is_const_v<parent_type>);
+            test(t,parent);
+        };
+        apply_by_element(test_, test_data);
+    }
 }
+
+TEST_CASE("test_basic_indexer_converter_specialization","test_data_accessor"){
+    using gtensor::basic_indexer;
+    using helpers_for_testing::apply_by_element;
+
+    //0parent,1convertor,2index,3expected
+    auto test_data = std::make_tuple(
+        std::make_tuple(std::vector<int>{1,2,3,0}, [](auto i){return i;}, 0, 1),
+        std::make_tuple(std::vector<int>{1,2,3,0}, [](auto i){return ++i;}, 1, 3),
+        std::make_tuple(std::vector<int>{1,2,3,0}, [](auto i){return --i;}, 2, 2),
+        std::make_tuple(std::vector<int>{1,2,3,0}, [](auto i){return i;}, 3, 0)
+    );
+    auto test = [](const auto& t, auto& parent){
+        auto converter = std::get<1>(t);
+        auto index = std::get<2>(t);
+        auto expected = std::get<3>(t);
+        using converter_type = decltype(converter);
+        using parent_type = decltype(parent);
+        static_assert(std::is_lvalue_reference_v<parent_type>);
+        auto indexer = basic_indexer<basic_indexer<parent_type>,converter_type>{basic_indexer<parent_type>{parent}, converter};
+        auto result = indexer[index];
+        REQUIRE(std::is_same_v<decltype(parent[index]),decltype(indexer[index])>);
+        REQUIRE(result == expected);
+    };
+    SECTION("test_basic_indexer_converter_specialization_non_const_parent")
+    {
+        auto test_ = [test](const auto& t){
+            auto parent = std::get<0>(t);
+            using parent_type = decltype(parent);
+            static_assert(!std::is_const_v<parent_type>);
+            test(t, parent);
+        };
+        apply_by_element(test_, test_data);
+    }
+    SECTION("test_basic_indexer_converter_specialization_const_parent")
+    {
+        auto test_ = [test](const auto& t){
+            const auto parent = std::get<0>(t);
+            using parent_type = decltype(parent);
+            static_assert(std::is_const_v<parent_type>);
+            test(t, parent);
+        };
+        apply_by_element(test_, test_data);
+    }
+}
+
