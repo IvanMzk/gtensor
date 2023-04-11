4366f686c24cc8b676704a7906e13d62b84da9a6
Ivan Malezhyk <ivanmzk@gmail.com>
2023-04-11 16:59:33 +0300

    refactor data_accessor module add walker

--- src/data_accessor.hpp
+++ src/data_accessor.hpp
@@ -5,6 +5,38 @@
 
 namespace gtensor{
 
+namespace detail{
+
+template<typename SizeT, typename IdxT>
+constexpr inline bool can_walk(const SizeT& direction, const SizeT& dim, const IdxT& direction_dim){
+    return direction < dim && direction_dim != IdxT(1);
+}
+
+template<typename CfgT>
+class shape_inverter
+{
+    using index_type = typename CfgT::index_type;
+    using shape_type = typename CfgT::shape_type;
+    using dim_type = typename CfgT::dim_type;
+
+    const index_type* shape_last;
+
+public:
+    shape_inverter(const shape_type& shape_):
+        shape_last{shape_.data()+shape_.size()-1}
+    {}
+
+    //direction must be in range [0,dim-1]
+    //0 direction corresponding to last shape element - direction with minimal stride
+    //1 direction corresponding to shape element befor last
+    //...
+    //dim-1 direction correcponding to 0 shape element - direction with max stride
+    index_type element(const dim_type& direction)const{return *(shape_last-direction);}
+};
+
+}   //end of namespace detail
+
+
 //basic indexer is data accessor that uses flat index to address data
 //indexers can be chained to make data view
 template<typename...> class basic_indexer;
@@ -64,6 +96,88 @@ public:
 private:
     const Converter_type* converter_;
 };
+
+//walker is indexer adapter that allows address data elements using multidimensional index
+//Cursor is responsible for storing flat position, it may have semantic of integral type or random access iterator
+template<typename Config, typename Cursor>
+class walker_common
+{
+public:
+    using cursor_type = Cursor;
+    using index_type = typename Config::index_type;
+    using dim_type = typename Config::dim_type;
+    using shape_type = typename Config::shape_type;
+
+    walker_common(const shape_type& adapted_strides__, const shape_type& reset_strides__, const cursor_type& offset__, const dim_type& max_dim__):
+        adapted_strides_{&adapted_strides__},
+        reset_strides_{&reset_strides__},
+        offset_{offset__},
+        cursor_{offset__},
+        dim_offset_{max_dim__ - adapted_strides__.size()}
+    {}
+    //direction argument must be in range [0,max_dim_-1]
+    void walk(const dim_type& direction, const index_type& steps){
+        if (direction >= dim_offset_){
+            cursor_+=steps*(*adapted_strides_)[direction - dim_offset_];
+        }
+    }
+    void step(const dim_type& direction){
+        if (direction >= dim_offset_){
+            cursor_+=(*adapted_strides_)[direction - dim_offset_];
+        }
+    }
+    void step_back(const dim_type& direction){
+        if (direction >= dim_offset_){
+            cursor_-=(*adapted_strides_)[direction - dim_offset_];
+        }
+    }
+    void reset(const dim_type& direction){
+        if (direction >= dim_offset_){
+            cursor_+=(*reset_strides_)[direction - dim_offset_];
+        }
+    }
+    void reset_back(const dim_type& direction){
+        if (direction >= dim_offset_){
+            cursor_-=(*reset_strides_)[direction - dim_offset_];
+        }
+    }
+    void reset_back(){cursor_ = offset_;}
+    cursor_type cursor()const{return cursor_;}
+    cursor_type offset()const{return offset_;}
+private:
+    const shape_type* adapted_strides_;
+    const shape_type* reset_strides_;
+    cursor_type offset_;
+    cursor_type cursor_;
+    dim_type dim_offset_;
+};
+
+//adapter of Indexer
+template<typename Config, typename Indexer>
+class walker
+{
+    using config_type = Config;
+    using index_type = typename config_type::index_type;
+    using dim_type = typename config_type::dim_type;
+    using shape_type = typename config_type::shape_type;
+    using indexer_type = Indexer;
+
+    walker_common<config_type, index_type> index_walker;
+    indexer_type indexer;
+public:
+    walker(const shape_type& adapted_strides_, const shape_type& reset_strides_, const index_type& offset_, const indexer_type& indexer_, const dim_type& max_dim_):
+        index_walker{adapted_strides_, reset_strides_, offset_, max_dim_},
+        indexer{indexer_}
+    {}
+    void walk(const dim_type& direction, const index_type& steps){index_walker.walk(direction,steps);}
+    void step(const dim_type& direction){index_walker.step(direction);}
+    void step_back(const dim_type& direction){index_walker.step_back(direction);}
+    void reset(const dim_type& direction){index_walker.reset(direction);}
+    void reset_back(const dim_type& direction){index_walker.reset_back(direction);}
+    void reset_back(){index_walker.reset_back();}
+    decltype(indexer[index_walker.cursor()]) operator*()const{return indexer[index_walker.cursor()];}
+};
+
 
 
 
--- test/test_data_accessor.cpp
+++ test/test_data_accessor.cpp
@@ -90,3 +90,127 @@ TEST_CASE("test_basic_indexer_converter_specialization","test_data_accessor"){
     }
 }
 
+TEST_CASE("test_walker","test_data_accessor"){
+    using value_type = int;
+    using config_type = gtensor::config::extend_config_t<gtensor::config::default_config,value_type>;
+    using gtensor::basic_indexer;
+    using gtensor::walker;
+    using shape_type = config_type::shape_type;
+    using dim_type = config_type::dim_type;
+    using index_type = config_type::index_type;
+    using storage_type = std::vector<value_type>;
+    using helpers_for_testing::apply_by_element;
+    //0storage,1adapted_strides,2reset_strides,3offset,4max_dim,5mover,6expected
+    auto test_data = std::make_tuple(
+        //1-d
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.step(0);}, value_type{2}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.step(0); w.step(0);}, value_type{3}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,0);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,1);}, value_type{2}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,2);}, value_type{3}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,5);}, value_type{6}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.step(0), w.walk(0,2);}, value_type{4}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,2); w.step(0);}, value_type{4}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.step(0); w.step_back(0);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,2); w.step_back(0);}, value_type{2}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,5); w.reset_back(0);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.reset(0);}, value_type{6}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.reset_back();}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{0}, dim_type{1}, [](auto& w){w.walk(0,2); w.step(0); w.reset_back();}, value_type{1}),
+        //1-d,offset,max_dim
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(1);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(2);}, value_type{2}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(0,2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,2);}, value_type{3}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(2); w.step_back(0);}, value_type{2}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(2); w.step_back(1);}, value_type{2}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.step(2); w.step_back(2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,5); w.reset_back(0);}, value_type{6}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,5); w.reset_back(1);}, value_type{6}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,5); w.reset_back(2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.reset(0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.reset(1);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.reset(2);}, value_type{6}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.reset_back();}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{1}, shape_type{5}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,5); w.reset_back();}, value_type{1}),
+        //2-d,offset,max_dim
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(1);}, value_type{4}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(2);}, value_type{2}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(2); w.step(1);}, value_type{5}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(1); w.step(2);}, value_type{5}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(0,2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,1);}, value_type{4}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,2);}, value_type{3}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(1); w.step_back(0);}, value_type{4}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(1); w.step_back(1);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.step(1); w.step_back(2);}, value_type{3}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,1); w.reset_back(0);}, value_type{4}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,1); w.reset_back(1);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,2); w.reset_back(0);}, value_type{3}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(2,2); w.reset_back(2);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.reset(0);}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.reset(1);}, value_type{4}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.reset(2);}, value_type{3}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.reset(1); w.reset(2);}, value_type{6}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.reset_back();}, value_type{1}),
+        std::make_tuple(storage_type{0,0,0,1,2,3,4,5,6}, shape_type{3,1}, shape_type{3,2}, index_type{3}, dim_type{3}, [](auto& w){w.walk(1,1); w.reset_back();}, value_type{1}),
+        //3-d,unit dimension size
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step(0);}, value_type{4}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step(1);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step(2);}, value_type{2}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step(2); w.step(1); w.step(0);}, value_type{5}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.walk(0,1);}, value_type{4}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.walk(1,1);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.walk(1,2);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.walk(2,2);}, value_type{3}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step_back(1);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.step(2); w.step_back(1);}, value_type{2}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.reset_back(1);}, value_type{1}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.walk(2,2); w.reset_back(1);}, value_type{3}),
+        std::make_tuple(storage_type{1,2,3,4,5,6}, shape_type{3,0,1}, shape_type{3,0,2}, index_type{0}, dim_type{3}, [](auto& w){w.reset(1);}, value_type{1})
+    );
+    auto test = [](const auto& t, auto& storage){
+        auto adapted_strides = std::get<1>(t);
+        auto reset_strides = std::get<2>(t);
+        auto offset = std::get<3>(t);
+        auto max_dim = std::get<4>(t);
+        auto mover = std::get<5>(t);
+        auto expected = std::get<6>(t);
+        using storage_type_ = decltype(storage);
+        using indexer_type = basic_indexer<storage_type_>;
+        using walker_type = walker<config_type, indexer_type>;
+        auto indexer = indexer_type{storage};
+        auto walker =  walker_type{adapted_strides,reset_strides,offset,indexer,max_dim};
+        mover(walker);
+        auto result = *walker;
+        REQUIRE(std::is_same_v<decltype(storage[std::declval<index_type>()]),decltype(*walker)>);
+        REQUIRE(result == expected);
+    };
+    SECTION("test_walker_non_const_storage")
+    {
+        auto test_ = [test](const auto& t){
+            auto storage = std::get<0>(t);
+            using storage_type_ = decltype(storage);
+            static_assert(!std::is_const_v<storage_type_>);
+            test(t, storage);
+        };
+        apply_by_element(test_, test_data);
+    }
+    SECTION("test_walker_const_storage")
+    {
+        auto test_ = [test](const auto& t){
+            const auto storage = std::get<0>(t);
+            using storage_type_ = decltype(storage);
+            static_assert(std::is_const_v<storage_type_>);
+            test(t, storage);
+        };
+        apply_by_element(test_, test_data);
+    }
+}
