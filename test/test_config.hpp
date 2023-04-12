be5825df8a70a7d62e1ce75f752e1c8c1e2ef7a0
Ivan Malezhyk <ivanmzk@gmail.com>
2023-04-12 11:52:22 +0300

    refactor test_config add config selector for specified storage

--- test/test_config.hpp
+++ test/test_config.hpp
@@ -4,28 +4,32 @@
 
 namespace test_config{
 
-template<typename Div, typename Eng>
+template<typename Div, typename Eng, template<typename> typename Stor>
 struct config_tmpl_{
     using engine = Eng;
     using div_mode = Div;
-    template<typename ValT> using storage = typename gtensor::config::default_config::storage<ValT>;
+    template<typename ValT> using storage = Stor<ValT>;
     template<typename ValT> using shape = typename gtensor::config::default_config::shape<ValT>;
     template<typename T> using container = typename gtensor::config::default_config::container<T>;
 };
 
-template<typename Eng, typename Div>
-struct config_engine_div_selector{
-    using config_type = config_tmpl_<Div,Eng>;
-};
+// template<typename Eng, typename Div>
+// struct config_engine_div_selector{
+//     using config_type = config_tmpl_<Div,Eng>;
+// };
 
-template<typename Eng>
-struct config_engine_selector{
-    using config_type = config_tmpl_<typename gtensor::config::default_config::div_mode,Eng>;
-};
+// template<typename Eng>
+// struct config_engine_selector{
+//     using config_type = config_tmpl_<typename gtensor::config::default_config::div_mode,Eng>;
+// };
 
 template<typename Div>
 struct config_div_mode_selector{
-    using config_type = config_tmpl_<Div,typename gtensor::config::default_config::engine>;
+    using config_type = config_tmpl_<Div,gtensor::config::default_config::engine,gtensor::config::default_config::template storage>;
+};
+template<template<typename> typename Stor>
+struct config_storage_selector{
+    using config_type = config_tmpl_<gtensor::config::default_config::div_mode,gtensor::config::default_config::engine,Stor>;
 };
 
 }   //end of namespace test_config
