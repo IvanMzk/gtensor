4c1dae77eabd83973bfb6d2dfe23034d9df3a184
Ivan Malezhyk <ivanmzk@gmail.com>
2023-07-24 15:33:02 +0300

    add manipultion test

--- test/test_manipulation.cpp
+++ test/test_manipulation.cpp
@@ -1043,6 +1043,9 @@ TEMPLATE_TEST_CASE("test_block_tuple","[test_manipulation]",
     using order = TestType;
     using value_type = double;
     using tensor_type = gtensor::tensor<value_type, order>;
+    using gtensor::tensor;
+    using gtensor::config::c_order;
+    using gtensor::config::f_order;
     using gtensor::block;
     using helpers_for_testing::apply_by_element;
 
@@ -1108,6 +1111,49 @@ TEMPLATE_TEST_CASE("test_block_tuple","[test_manipulation]",
         std::make_tuple(std::make_tuple(
             std::make_tuple(std::make_tuple(tensor_type{{{1}},{{2}}}, tensor_type{{{3}},{{4}}}), std::make_tuple(tensor_type{{{5,6},{7,8}},{{9,10},{11,12}}})),
             std::make_tuple(std::make_tuple(tensor_type{13,14}), std::make_tuple(tensor_type{15,16}), std::make_tuple(tensor_type{17,18}))),
+            tensor_type{{{1,3},{5,6},{7,8}},{{2,4},{9,10},{11,12}},{{13,14},{15,16},{17,18}}}
+        )
+    );
+    auto test = [](const auto& t){
+        auto blocks = std::get<0>(t);
+        auto expected = std::get<1>(t);
+        auto result = block(blocks);
+        REQUIRE(result == expected);
+    };
+    apply_by_element(test, test_data);
+}
+
+TEST_CASE("test_block_tuple_common_order","[test_manipulation]")
+{
+    using value_type = double;
+    using tensor_type = gtensor::tensor<value_type>;
+    using gtensor::tensor;
+    using gtensor::config::c_order;
+    using gtensor::config::f_order;
+    using gtensor::block;
+    using helpers_for_testing::apply_by_element;
+
+    //0blocks,1expected
+    auto test_data = std::make_tuple(
+        std::make_tuple(std::make_tuple(
+            std::make_tuple(tensor<value_type,c_order>{{1,2},{3,4}}, tensor<value_type,f_order>{{5,6,7},{8,9,10}}), std::make_tuple(tensor<value_type,c_order>{{11,12,13,14,15}})),
+            tensor_type{{1,2,5,6,7},{3,4,8,9,10},{11,12,13,14,15}}
+        ),
+        std::make_tuple(std::make_tuple(
+            std::make_tuple(tensor<value_type,f_order>{{1,2},{3,4}}, tensor<value_type,f_order>{{5,6},{7,8}}), std::make_tuple(tensor<value_type,c_order>{{9},{10}}, tensor<value_type,c_order>{{11,12,13},{14,15,16}})),
+            tensor_type{{1,2,5,6},{3,4,7,8},{9,11,12,13},{10,14,15,16}}
+        ),
+        std::make_tuple(std::make_tuple(
+            std::make_tuple(tensor<value_type,c_order>{{1,2},{3,4}}), std::make_tuple(tensor<value_type,f_order>{{5,6},{7,8}}), std::make_tuple(tensor<value_type,c_order>{{9},{10}}, tensor<value_type,f_order>{{11},{12}})),
+            tensor_type{{1,2},{3,4},{5,6},{7,8},{9,11},{10,12}}
+        ),
+        std::make_tuple(std::make_tuple(
+            std::make_tuple(tensor<value_type,f_order>{{{1}},{{2}}}, tensor<value_type,c_order>{{{3}},{{4}}}), std::make_tuple(tensor<value_type,c_order>{{{5,6},{7,8}},{{9,10},{11,12}}})),
+            tensor_type{{{1,3},{5,6},{7,8}},{{2,4},{9,10},{11,12}}}
+        ),
+        std::make_tuple(std::make_tuple(
+            std::make_tuple(std::make_tuple(tensor<value_type,c_order>{{{1}},{{2}}}, tensor<value_type,f_order>{{{3}},{{4}}}), std::make_tuple(tensor<value_type,c_order>{{{5,6},{7,8}},{{9,10},{11,12}}})),
+            std::make_tuple(std::make_tuple(tensor<value_type,f_order>{13,14}), std::make_tuple(tensor<value_type,c_order>{15,16}), std::make_tuple(tensor<value_type,f_order>{17,18}))),
             tensor_type{{{1,3},{5,6},{7,8}},{{2,4},{9,10},{11,12}},{{13,14},{15,16},{17,18}}}
         )
     );
