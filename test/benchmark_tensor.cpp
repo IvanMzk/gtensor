2bfafc3d8a9d0e22c7004414ff841b6bf973a78c
Ivan Malezhyk <ivanmzk@gmail.com>
2023-08-05 23:01:11 +0300

    refactor benchmark

--- test/benchmark_tensor.cpp
+++ test/benchmark_tensor.cpp
@@ -5,22 +5,22 @@ namespace benchmark_expression_template_helpers{
 }   //end of namespace benchmark_expression_template_helpers
 
 
-TEST_CASE("test_benchmark_helpers_make_tree","[test_benchmark_helpers]")
-{
-    using value_type = double;
-    using tensor_type = gtensor::tensor<value_type>;
-    using benchmark_helpers::make_asymmetric_tree;
+// TEST_CASE("test_benchmark_helpers_make_tree","[test_benchmark_helpers]")
+// {
+//     using value_type = double;
+//     using tensor_type = gtensor::tensor<value_type>;
+//     using benchmark_helpers::make_asymmetric_tree;
 
-    REQUIRE(make_asymmetric_tree<1>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{2,4,6},{8,10,12}});
-    REQUIRE(make_asymmetric_tree<1>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{8,10,12},{11,13,15}});
-    REQUIRE(make_asymmetric_tree<2>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{3,6,9},{12,15,18}});
-    REQUIRE(make_asymmetric_tree<2>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{15,18,21},{18,21,24}});
-    REQUIRE(make_asymmetric_tree<3>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{4,8,12},{16,20,24}});
-    REQUIRE(make_asymmetric_tree<3>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{22,26,30},{25,29,33}});
-    REQUIRE(make_asymmetric_tree<4>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{5,10,15},{20,25,30}});
-    REQUIRE(make_asymmetric_tree<4>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{29,34,39},{32,37,42}});
+//     REQUIRE(make_asymmetric_tree<1>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{2,4,6},{8,10,12}});
+//     REQUIRE(make_asymmetric_tree<1>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{8,10,12},{11,13,15}});
+//     REQUIRE(make_asymmetric_tree<2>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{3,6,9},{12,15,18}});
+//     REQUIRE(make_asymmetric_tree<2>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{15,18,21},{18,21,24}});
+//     REQUIRE(make_asymmetric_tree<3>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{4,8,12},{16,20,24}});
+//     REQUIRE(make_asymmetric_tree<3>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{22,26,30},{25,29,33}});
+//     REQUIRE(make_asymmetric_tree<4>(tensor_type{{1,2,3},{4,5,6}},tensor_type{{1,2,3},{4,5,6}}) == tensor_type{{5,10,15},{20,25,30}});
+//     REQUIRE(make_asymmetric_tree<4>(tensor_type{{1,2,3},{4,5,6}},tensor_type{7,8,9}) == tensor_type{{29,34,39},{32,37,42}});
 
-}
+// }
 
 // TEMPLATE_TEST_CASE("benchmark_expression_view_iterator","[benchmark_tensor]",
 //     gtensor::config::c_order,
@@ -229,39 +229,7 @@ TEST_CASE("test_benchmark_helpers_make_tree","[test_benchmark_helpers]")
 //     // benchmark("f_copy_slice3_view_10E6",bench_copy,v3,f_order{});
 // }
 
-// TEMPLATE_TEST_CASE("benchmark_tensor_reduce","[benchmark_tensor]",
-//     gtensor::config::c_order,
-//     gtensor::config::f_order
-// )
-// {
-//     using value_type = double;
-//     using gtensor::tensor;
-//     using gtensor::config::c_order;
-//     using gtensor::config::f_order;
-//     using tensor_type = gtensor::tensor<value_type,TestType>;
-//     using benchmark_helpers::make_asymmetric_tree;
-//     using benchmark_helpers::benchmark;
-
-//     auto bench_sum = [](const auto& t, auto...axes){
-//         if constexpr (sizeof...(axes)==0){
-//             auto tmp = t.sum();
-//             return tmp.size();
-//         }else{
-//             auto tmp = t.sum({axes...});
-//             return tmp.size();
-//         }
-//     };
-
-//     auto t = tensor_type({2,5,100,1000},2);
-
-//     benchmark("sum_all_10E6",bench_sum,t);
-//     benchmark("sum_axis_10E6",bench_sum,t,1);
-//     benchmark("sum_axes2_10E6",bench_sum,t,0,2);
-//     benchmark("sum_axes3_10E6",bench_sum,t,0,1,2);
-//     benchmark("sum_axes4_10E6",bench_sum,t,0,1,2,3);
-// }
-
-TEMPLATE_TEST_CASE("benchmark_tensor_argmax","[benchmark_tensor]",
+TEMPLATE_TEST_CASE("benchmark_tensor_reduce","[benchmark_tensor]",
     gtensor::config::c_order,
     gtensor::config::f_order
 )
@@ -274,22 +242,69 @@ TEMPLATE_TEST_CASE("benchmark_tensor_argmax","[benchmark_tensor]",
     using benchmark_helpers::make_asymmetric_tree;
     using benchmark_helpers::benchmark;
 
-    auto bench_argmax = [](const auto& t, auto...axes){
+    auto bench_sum = [](const auto& t, auto...axes){
         if constexpr (sizeof...(axes)==0){
-            auto tmp = t.argmax();
+            auto tmp = t.sum();
             return tmp.size();
         }else{
-            auto tmp = t.argmax({axes...});
+            auto tmp = t.sum({axes...});
             return tmp.size();
         }
     };
 
-    auto t = tensor_type({2,5,100,1000},2);
+    //auto t = tensor_type({10,5,100,1000},2);
+    auto t = tensor_type({50,50,50,50},2);
 
-    benchmark("argmax_all_10E6",bench_argmax,t);
-    benchmark("argmax_axis_10E6",bench_argmax,t,1);
-    // benchmark("argmax_axes2_10E6",bench_argmax,t,0,2);
-    // benchmark("argmax_axes3_10E6",bench_argmax,t,0,1,2);
-    // benchmark("argmax_axes4_10E6",bench_argmax,t,0,1,2,3);
+    //like over flatten
+    //benchmark("sum_all_10E6",bench_sum,t);
+
+    //single axis
+    // benchmark("sum_axis_10E6",bench_sum,t,0);
+    // benchmark("sum_axis_10E6",bench_sum,t,1);
+    // benchmark("sum_axis_10E6",bench_sum,t,2);
+    // benchmark("sum_axis_10E6",bench_sum,t,3);
+
+    //axes
+    benchmark("sum_axes_0_1_10E6",bench_sum,t,0,1);
+    benchmark("sum_axes_0_2_10E6",bench_sum,t,0,2);
+    benchmark("sum_axes_0_3_10E6",bench_sum,t,0,3);
+    benchmark("sum_axes_1_2_10E6",bench_sum,t,1,2);
+    benchmark("sum_axes_1_3_10E6",bench_sum,t,1,3);
+    benchmark("sum_axes_2_3_10E6",bench_sum,t,2,3);
+
+    //benchmark("sum_axes3_10E6",bench_sum,t,0,1,2);
+    //benchmark("sum_axes4_10E6",bench_sum,t,0,1,2,3);
 }
+
+// TEMPLATE_TEST_CASE("benchmark_tensor_argmax","[benchmark_tensor]",
+//     gtensor::config::c_order,
+//     gtensor::config::f_order
+// )
+// {
+//     using value_type = double;
+//     using gtensor::tensor;
+//     using gtensor::config::c_order;
+//     using gtensor::config::f_order;
+//     using tensor_type = gtensor::tensor<value_type,TestType>;
+//     using benchmark_helpers::make_asymmetric_tree;
+//     using benchmark_helpers::benchmark;
+
+//     auto bench_argmax = [](const auto& t, auto...axes){
+//         if constexpr (sizeof...(axes)==0){
+//             auto tmp = t.argmax();
+//             return tmp.size();
+//         }else{
+//             auto tmp = t.argmax({axes...});
+//             return tmp.size();
+//         }
+//     };
+
+//     auto t = tensor_type({2,5,100,1000},2);
+
+//     benchmark("argmax_all_10E6",bench_argmax,t);
+//     benchmark("argmax_axis_10E6",bench_argmax,t,1);
+//     // benchmark("argmax_axes2_10E6",bench_argmax,t,0,2);
+//     // benchmark("argmax_axes3_10E6",bench_argmax,t,0,1,2);
+//     // benchmark("argmax_axes4_10E6",bench_argmax,t,0,1,2,3);
+// }
 
